987e853d4bee9e35bc6128982786d7246139b95a
zhaoolee <zhaoolee@gmail.com>
2021-01-02 13:17:51 +0800

    ok

--- client/typora.js
+++ client/typora.js
@@ -2,26 +2,36 @@ const process = require('process');
 
 var request = require('request');
 const fs = require("fs-extra");
-const { argv } = require('process');
+
+const conf_info = require("../conf");
 
 
-// 请求的接口
+async function upload_file(local_image_path) {
 
-const upload_api = "http://localhost:8080/upload_file"
-
-async function upload_img(local_image_path) {
-
-    const FormData = {
-        file: fs.createReadStream(local_image_path)
+    // 获取配置信息
+    const host = conf_info["host"];
+    const client_port = conf_info["client_port"];
+    const secret_token = conf_info["secret_token"];
+    const url = `${host}:${client_port}/upload_file`;
+    const formData = {
+        file: fs.createReadStream(local_image_path),
+        secret_token: secret_token
+        
     };
 
-    // console.log("FormData==>", FormData);
-
     return new Promise(async (resolve, reject)=>{
-        await request.post({url: upload_api,formData: FormData}, function optionalCallback(err, httpResponse, body) {
+        await request.post({url: url, formData: formData}, function optionalCallback(err, httpResponse, body) {
 
-            // console.log("===body=>>", body)
-            resolve(body);
+            if(err){
+                // 如果请求出错，则返回一张猫爪图
+                console.log("\nerr==>>", err, "\nhttpResponse==>>", httpResponse, "\nbody==>>",body, "\nurl===>>", url);
+                resolve("https://www.v2fy.com/wp-content/uploads/2020/05/keycat1000.jpg")
+            }else{
+                // 如果请求正常，则返回图片地址
+                resolve(body);
+            }
+
+            
         })
         
     })
@@ -31,17 +41,16 @@ async function upload_img(local_image_path) {
 
 
 async function main(){
+
     // 获取参数列表
     const args = process.argv.slice(2);
 
     let result = "Upload Success:";
-
     for(let i =0, args_length = args.length; i<args_length; i++){
-        image_url = await upload_img(args[i])
+        image_url = await upload_file(args[i])
         result = result + "\n"+image_url;
     }
     console.log(result);
-
     return result;
 
 }
--- server/index.js
+++ server/index.js
@@ -2,47 +2,89 @@ const express = require("express");
 const multer = require("multer");
 const expressStatic = require("express-static");
 const fs = require("fs-extra");
+const matter = require('gray-matter');
+const path = require("path");
+const moment = require("moment");
+const conf_info = require("../conf");
 
 let server = express();
-let upload = multer({ dest: __dirname +'/uploads/' });
-let path = require("path");
+let upload = multer({ dest: __dirname + '/assets/' });
 
-// hosts
-let server_hosts = "http://localhost:8080"
+
+// 获取配置信息
+const host = (conf_info)["host"];
+const client_port = (conf_info)["client_port"];
+const server_port = (conf_info)["server_port"];
+const secret_token = (conf_info)["secret_token"];
+const url = `${host}:${client_port}/upload_file`;
+
+console.log(`host:${host}`);
+console.log(`client_port:${client_port}`);
+console.log(`server_port:${server_port}`);
+console.log(`url:${url}`);
+
+
 
 //  随机产生字符串
-
-function randomString(e) {    
+function randomString(e) {
   e = e || 32;
   var t = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz012345678",
-  a = t.length,
-  n = "";
+    a = t.length,
+    n = "";
   for (i = 0; i < e; i++) n += t.charAt(Math.floor(Math.random() * a));
   return n
 }
-
 
 // 处理提交文件的post请求
 server.post('/upload_file', upload.single('file'), function (req, res) {
 
   console.log("file信息", req.file);
+  let new_image_name = String(Date.now()) + randomString(8) + "." + req.file.mimetype.split("/").pop();
+  console.log("===req>>>::", req);
+  // 如配置文件conf.js中secret_token为空字符串，或客户端携带正确的secret_token, 则进行存储； 如果无法通过校验，则返回鉴权失败
+  if((secret_token.length === 0) || (req["body"]["secret_token"] === secret_token)){
 
-  let new_image_name = String(Date.now())+randomString(8)+"."+req.file.mimetype.split("/").pop();
+    fs.moveSync(req.file.path, path.join(req.file.destination, new_image_name));
+    let image_url = `${host}:${client_port}/assets/${new_image_name}`;
+    if(client_port == "80"){
+      image_url = `${host}/assets/${new_image_name}`;
+    }
   
+    console.log(`--${moment().format("YYYY-MM-DD HH:mm:ss")}-image_url-->>${image_url}`)
+    res.send(image_url)
 
-  fs.moveSync(req.file.path, path.join(req.file.destination, new_image_name));
+  }else{
 
-  console.log("--image-new-url-->>", server_hosts+"/"+new_image_name)
+    res.send("鉴权失败！请重新运行npm run build构建新的私有客户端")
 
-  res.send(server_hosts+"/"+new_image_name)
+  }
+
+
 
 })
 
+server.get("/info", function(req, res){
+  
+  res.send({
+    host: host,
+    client_port: client_port,
+    server_port: server_port,
+    url: url
+  })
+})
+
+
+server.get("/", function(req, res){
+  
+  res.send("这是一个适用于Typora的私有化图床！")
+})
+
 // 处理静态目录
-server.use(expressStatic(path.join(__dirname, "uploads")));
+server.use(expressStatic(__dirname));
+
 
 
 // 监听服务
-server.listen(8080, function(){
-  console.log("请使用浏览器访问 "+server_hosts)
+server.listen(server_port, function () {
+  console.log(`图床上传接口 ${url}`)
 });
